04bf9fee53175314e6ee8c1fa277ecc57371aa14
dllee <dllee@99-DTX3NR>
2022-02-14 13:45:08 -0500

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using Twitch.Models;
+
+namespace Twitch.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+
+            
+        }
+
+        
+
+        public IActionResult Index(UserModel u)
+        {
+         
+            
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+           
+            return View();
+        }
+      
+     
+
+        public class UserModel
+        {
+            public string Suggestion { get; set; }
+        }
+
+        public class suggestionButton
+        {
+            public string suggestionType { get; set; }
+
+        }
+
+       
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Twitch.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
